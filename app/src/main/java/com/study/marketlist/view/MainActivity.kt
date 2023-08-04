080a46c9e88edcbcba18d6ece1e4f7b13557114a
EndrylCristian <endrylfiorotti.accenture@natura.net>
2023-08-04 15:28:33 -0300

    :construction: Criação do layout.

--- app/src/main/java/com/study/marketlist/view/MainActivity.kt
+++ app/src/main/java/com/study/marketlist/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.study.marketlist
+package com.study.marketlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.study.marketlist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
